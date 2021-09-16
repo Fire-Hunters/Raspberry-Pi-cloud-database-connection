57bf441b09812964ad024c475c98e97455d5d101
MertbabaOkulmus <mertbaba.okulmus@hotmail.com>
2021-09-16 21:49:02 +0300

    txt den  0 gelme durumu eklendi sistemin sürekli otonom olarak çalışması için setInterval metodu eklendi /

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import fs from 'fs';
-import {gonder} from './mail.js';
-import {insert} from './insert.js';
+import { gonder } from './mail.js';
+import { update } from './update.js';
 
-fs.readFile('durum.txt', 'utf-8',async function (err, data) {
 
-    if (err) throw err;
-    if (String(data) == 1) {
-    // await   gonder()
-insert()
-    }
-});
+const islem = () => {
+    fs.readFile('durum.txt', 'utf-8', async function (err, data) {
+        console.log(Date.now)
+        if (err) throw err;
+        if (String(data) == 1) {
+            await gonder()
+            await update(data)
+        }
+        else {
+            await update(data)
+        }
+    });
+}
+
+setInterval(islem,10000)
